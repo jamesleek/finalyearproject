95b117cc53519227417ce5c920411559177f7110
James Leek <jameswilliamleek@gmail.com>
2017-04-23 18:25:34 +0100

    Imported SweetAlert, Allowed Creation of .csv files
    Imporeted nuget package for sweet alert. Allowed creation of .csv fiels
    to be used in d3.js for bubble charts.

---
+++ FinalYearProject/Controllers/BubbleChartsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalYearProject.Models;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace FinalYearProject.Controllers
+{
+    public class BubbleChartsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: BubbleCharts
+        public ActionResult Index()
+        {
+            CreateCSVFiles();
+            return View(db.Categories.ToList());
+        }
+
+
+        //Create .CSV files for each charrt
+        public void CreateCSVFiles()
+        {
+
+            string directory = Server.MapPath("~/App_Data/");
+            string filename = "categories.csv";
+            string path = Path.Combine(directory, filename);
+
+
+            using (var sw = new StreamWriter(path))
+            {
+                sw.WriteLine("\"Category Name\",\"Number Of Bugs\"");
+
+                foreach (var item in db.Categories)
+                {
+                    sw.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        item.CategoryName, item.NumberOfBugs));
+                }
+
+            }
+           
+          
+        }
+
+        // GET: BubbleCharts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: BubbleCharts/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: BubbleCharts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CategoryName,Keywords,NumberOfBugs")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: BubbleCharts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: BubbleCharts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CategoryName,Keywords,NumberOfBugs")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: BubbleCharts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: BubbleCharts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
