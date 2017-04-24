c3c546f5e1072f54c5ceb32ce0b0b8582409eca1
James Leek <jameswilliamleek@gmail.com>
2017-04-24 18:46:43 +0100

    Added bubble graphs for months
    Added bubble graphs for months
    Also trying to get bubble graphs for user working

--- FinalYearProject/Controllers/BubbleChartsController.cs
+++ FinalYearProject/Controllers/BubbleChartsController.cs
@@ -44,8 +44,118 @@ namespace FinalYearProject.Controllers
                 }
 
             }
-           
-          
+
+            string directory2 = Server.MapPath("~/");
+            string filename2= "months.csv";
+            string path2 = Path.Combine(directory2, filename2);
+            int jan = 0;
+            int feb = 0;
+            int mar = 0;
+            int apr = 0;
+            int may = 0;
+            int jun = 0;
+            int jul = 0;
+            int aug = 0;
+            int sep = 0;
+            int oct = 0;
+            int nov = 0;
+            int dec = 0;
+
+
+            using (var sw2 = new StreamWriter(path2))
+            {
+                sw2.WriteLine("\"id\",\"value\"");
+
+                foreach (var item2 in db.BugReports)
+                {
+                    switch (item2.DateAdded.Month)
+                    {
+                        case (1):
+                            jan += 1;
+                            break;
+                        case (2):                            
+                            feb += 1;
+                            break;
+                        case (3):                           
+                            mar += 1;
+                            break;
+                        case (4):                            
+                            apr += 1;
+                            break;
+                        case (5):
+                            may += 1;
+                            break;
+                        case (6):
+                            jun += 1;
+                            break;
+                        case (7):
+                            jul += 1;
+                            break;
+                        case (8):
+                            aug += 1;
+                            break;
+                        case (9):
+                            sep += 1;
+                            break;
+                        case (10):
+                            oct += 1;
+                            break;
+                        case (11):
+                            nov += 1;
+                            break;
+                        case (12):
+                            dec += 1;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "Janurary", jan));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "February", feb));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "March", mar));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "April", apr));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "May", may));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "June", jun));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "July", jul));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "Aug", aug));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "September", sep));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "October", oct));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "November", nov));
+                sw2.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                        "December", dec));
+
+            }
+
+            string directory3 = Server.MapPath("~/");
+            string filename3 = "BugsByUser.csv";
+            string path3 = Path.Combine(directory3, filename3);
+            
+
+            using(var sw3 = new StreamWriter(path3))
+            {
+                sw3.WriteLine("\"id\",\"value\"");
+
+                foreach(var item3 in db.BugReports)
+                {
+                    
+                    sw3.WriteLine(string.Format("\"{0}\",\"{1}\"",
+                         item3.User.Email.ToString(), 1));
+                }
+            }
+
+
         }
 
         // GET: BubbleCharts/Details/5
